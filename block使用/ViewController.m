92ea725a36c2b409e314cf0bfb9b3f6ed8f6e880
XJL <xjl@XJLdeMacBook-Pro.local>
2018-11-04 16:11:12 +0800

    block使用

--- block使用/ViewController.m
+++ block使用/ViewController.m
@@ -17,8 +17,28 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
-}
+  
+  //block的定义
+  // 返回类型（^block名称)(参数）=^{表达式}
+ __block int count=10;
+  void (^blockName)(void)=^(){
+   
+    NSLog(@"%d",count);
+  };
+  count=9;
+  blockName();
+  
 
+  
+  
+  
+}
+void exampleA() {
+  char a = 'A';
+  ^{
+    printf("%c\n", a);
+  }();
+}
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
